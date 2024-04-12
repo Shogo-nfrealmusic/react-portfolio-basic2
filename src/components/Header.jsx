419a8064db70a471400e2f59ab5dff59cc0b1d5f
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-04-12 17:35:08 +0900

    resolve npm

---
+++ src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+        <div className="logo">
+            <h3>Portfolio</h3>
+        </div>
+
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/blog">Blog</Link>
+                </li>
+                <li>
+                    <a href="#">SNS</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Header
