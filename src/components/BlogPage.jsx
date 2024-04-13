7924da5dcb726dfe25b79bc13759cbcae4e46dbf
Shogo-nfrealmusic <shogo.baseball0306@gmail.com>
2024-04-13 15:06:44 +0900

    finish making portfolio site with using react.js

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -2,7 +2,69 @@ import React from 'react';
 
 const BlogPage = () => {
   return (
-    <div>BlogPage</div>
+    <div className='container'>
+      <h1>Blog</h1>
+      <p> 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dolorem nihil delectus ipsa, 
+          architecto voluptatem earum adipisci, eligendi eos maiores enim recusandae ipsum culpa porro hic obcaecati. 
+          Labore sunt laboriosam dolorem sed quasi officia, nulla harum voluptate eligendi, ratione tenetur a dicta veniam modi rem,
+          voluptates laborum accusamus dignissimos deleniti reiciendis illum qui! 
+          Corporis laboriosam suscipit, maxime doloribus unde dolores nam minus facilis.
+          Quas vitae nobis modi deserunt reiciendis qui rem iure voluptatum maiores magnam? Beatae, 
+          explicabo error omnis adipisci cupiditate culpa deleniti nostrum cum, corrupti dolore voluptates enim perferendis.
+          A velit asperiores culpa quidem in vel qui cum inventore.
+      </p>
+      <p> 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dolorem nihil delectus ipsa, 
+          architecto voluptatem earum adipisci, eligendi eos maiores enim recusandae ipsum culpa porro hic obcaecati. 
+          Labore sunt laboriosam dolorem sed quasi officia, nulla harum voluptate eligendi, ratione tenetur a dicta veniam modi rem,
+          voluptates laborum accusamus dignissimos deleniti reiciendis illum qui! 
+          Corporis laboriosam suscipit, maxime doloribus unde dolores nam minus facilis.
+          Quas vitae nobis modi deserunt reiciendis qui rem iure voluptatum maiores magnam? Beatae, 
+          explicabo error omnis adipisci cupiditate culpa deleniti nostrum cum, corrupti dolore voluptates enim perferendis.
+          A velit asperiores culpa quidem in vel qui cum inventore.
+      </p>
+      <p> 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dolorem nihil delectus ipsa, 
+          architecto voluptatem earum adipisci, eligendi eos maiores enim recusandae ipsum culpa porro hic obcaecati. 
+          Labore sunt laboriosam dolorem sed quasi officia, nulla harum voluptate eligendi, ratione tenetur a dicta veniam modi rem,
+          voluptates laborum accusamus dignissimos deleniti reiciendis illum qui! 
+          Corporis laboriosam suscipit, maxime doloribus unde dolores nam minus facilis.
+          Quas vitae nobis modi deserunt reiciendis qui rem iure voluptatum maiores magnam? Beatae, 
+          explicabo error omnis adipisci cupiditate culpa deleniti nostrum cum, corrupti dolore voluptates enim perferendis.
+          A velit asperiores culpa quidem in vel qui cum inventore.
+      </p>
+      <p> 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dolorem nihil delectus ipsa, 
+          architecto voluptatem earum adipisci, eligendi eos maiores enim recusandae ipsum culpa porro hic obcaecati. 
+          Labore sunt laboriosam dolorem sed quasi officia, nulla harum voluptate eligendi, ratione tenetur a dicta veniam modi rem,
+          voluptates laborum accusamus dignissimos deleniti reiciendis illum qui! 
+          Corporis laboriosam suscipit, maxime doloribus unde dolores nam minus facilis.
+          Quas vitae nobis modi deserunt reiciendis qui rem iure voluptatum maiores magnam? Beatae, 
+          explicabo error omnis adipisci cupiditate culpa deleniti nostrum cum, corrupti dolore voluptates enim perferendis.
+          A velit asperiores culpa quidem in vel qui cum inventore.
+      </p>
+      <p> 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dolorem nihil delectus ipsa, 
+          architecto voluptatem earum adipisci, eligendi eos maiores enim recusandae ipsum culpa porro hic obcaecati. 
+          Labore sunt laboriosam dolorem sed quasi officia, nulla harum voluptate eligendi, ratione tenetur a dicta veniam modi rem,
+          voluptates laborum accusamus dignissimos deleniti reiciendis illum qui! 
+          Corporis laboriosam suscipit, maxime doloribus unde dolores nam minus facilis.
+          Quas vitae nobis modi deserunt reiciendis qui rem iure voluptatum maiores magnam? Beatae, 
+          explicabo error omnis adipisci cupiditate culpa deleniti nostrum cum, corrupti dolore voluptates enim perferendis.
+          A velit asperiores culpa quidem in vel qui cum inventore.
+      </p>
+      <p> 
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio dolorem nihil delectus ipsa, 
+          architecto voluptatem earum adipisci, eligendi eos maiores enim recusandae ipsum culpa porro hic obcaecati. 
+          Labore sunt laboriosam dolorem sed quasi officia, nulla harum voluptate eligendi, ratione tenetur a dicta veniam modi rem,
+          voluptates laborum accusamus dignissimos deleniti reiciendis illum qui! 
+          Corporis laboriosam suscipit, maxime doloribus unde dolores nam minus facilis.
+          Quas vitae nobis modi deserunt reiciendis qui rem iure voluptatum maiores magnam? Beatae, 
+          explicabo error omnis adipisci cupiditate culpa deleniti nostrum cum, corrupti dolore voluptates enim perferendis.
+          A velit asperiores culpa quidem in vel qui cum inventore.
+      </p>
+    </div>
   )
 }
 
